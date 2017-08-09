963aa85ed28edc2323dfdcee6b6935f15938432b
robojones <jonathan.scholz-weil@t-online.de>
2017-08-09 12:36:02 +0200

    add eslint config

--- BetterEvents.js
+++ BetterEvents.js
@@ -1,96 +1,95 @@
 const EventEmitter = require('events')
 
 class BetterEvents extends EventEmitter {
-    static once(source, eventName, arrayMode) {
-
-        if(!(source instanceof EventEmitter)) {
-            return Promise.reject(new TypeError('source must be an instance of EventEmitter'))
-        }
-
-        if(source instanceof BetterEvents) {
-            return source.once(eventName, !!arrayMode)
-        }
-
-        if(arrayMode) {
-            return new Promise(resolve => {
-                source.once(eventName, (...args) => {
-                    resolve(args)
-                })
-            })
-        } else {
-            return new Promise(resolve => {
-                source.once(eventName, resolve)
-            })
-        }
+  static once(source, eventName, arrayMode) {
+    if (!(source instanceof EventEmitter)) {
+      return Promise.reject(new TypeError('source must be an instance of EventEmitter'))
     }
 
-    once(eventName, listener) {
-        if(typeof listener === 'function') {
-            return super.once(eventName, listener)
-        }
+    if (source instanceof BetterEvents) {
+      return source.once(eventName, !!arrayMode)
+    }
 
-        if (listener === true) {
-            //promise mode with array
-
-            return new Promise(resolve => {
-                super.once(eventName, (...args) => {
-                    resolve(args)
-                })
-            })
-        }
-
-        return new Promise(resolve => {
-            super.once(eventName, resolve)
+    if (arrayMode) {
+      return new Promise(resolve => {
+        source.once(eventName, (...args) => {
+          resolve(args)
         })
+      })
+    } else {
+      return new Promise(resolve => {
+        source.once(eventName, resolve)
+      })
+    }
+  }
+
+  once(eventName, listener) {
+    if (typeof listener === 'function') {
+      return super.once(eventName, listener)
     }
 
-    collect(eventName, source) {
-        if(!(source && source instanceof EventEmitter)) {
-            throw new TypeError('source must be an instance of EventEmitter')
-        }
+    if (listener === true) {
+      // promise mode with array
 
-        const cb = this.emit.bind(this, eventName)
-
-        source.on(eventName, cb)
-
-        return cb
+      return new Promise(resolve => {
+        super.once(eventName, (...args) => {
+          resolve(args)
+        })
+      })
     }
 
-    collectOnce(eventName, source) {
-        if(!(source && source instanceof EventEmitter)) {
-            throw new TypeError('source must be an instance of EventEmitter')
-        }
+    return new Promise(resolve => {
+      super.once(eventName, resolve)
+    })
+  }
 
-        const cb = this.emit.bind(this, eventName)
-
-        source.once(eventName, cb)
-
-        return cb
+  collect(eventName, source) {
+    if (!(source && source instanceof EventEmitter)) {
+      throw new TypeError('source must be an instance of EventEmitter')
     }
 
-    share(eventName, target) {
-        if(!(target && target instanceof EventEmitter)) {
-            throw new TypeError('target must be an instance of EventEmitter')
-        }
+    const cb = this.emit.bind(this, eventName)
 
-        const cb = target.emit.bind(target, eventName)
+    source.on(eventName, cb)
 
-        this.on(eventName, cb)
+    return cb
+  }
 
-        return cb
+  collectOnce(eventName, source) {
+    if (!(source && source instanceof EventEmitter)) {
+      throw new TypeError('source must be an instance of EventEmitter')
     }
 
-    shareOnce(eventName, target) {
-        if(!(target && target instanceof EventEmitter)) {
-            throw new TypeError('target must be an instance of EventEmitter')
-        }
+    const cb = this.emit.bind(this, eventName)
 
-        const cb = target.emit.bind(target, eventName)
+    source.once(eventName, cb)
 
-        this.once(eventName, cb)
+    return cb
+  }
 
-        return cb
+  share(eventName, target) {
+    if (!(target && target instanceof EventEmitter)) {
+      throw new TypeError('target must be an instance of EventEmitter')
     }
+
+    const cb = target.emit.bind(target, eventName)
+
+    this.on(eventName, cb)
+
+    return cb
+  }
+
+  shareOnce(eventName, target) {
+    if (!(target && target instanceof EventEmitter)) {
+      throw new TypeError('target must be an instance of EventEmitter')
+    }
+
+    const cb = target.emit.bind(target, eventName)
+
+    this.once(eventName, cb)
+
+    return cb
+  }
 }
 
 module.exports = BetterEvents
