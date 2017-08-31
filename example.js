2f968ce5aafb008b5e0d9c7f84896aff8967fa9f
robojones <jonathan.scholz-weil@t-online.de>
2017-08-31 18:58:09 +0200

    add example

---
+++ example.js
@@ -0,0 +1,22 @@
+const {
+  once,
+  shareEvent,
+  BetterEvents
+} = require('better-events')
+
+const emitter1 = new BetterEvents()
+const emitter2 = new BetterEvents()
+
+async function example() {
+  // If emitter1 emits "go" it will also be emitted by emitter2.
+  emitter1.share('go', emitter2)
+
+  // Await the "go" event.
+  await emitter2.once('go')
+
+  console.log("received event 'go'")
+}
+
+example().catch(console.error)
+
+emitter1.emit('go')
